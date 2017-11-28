89bdcefe775c1699503438fb5001401dcf097f22
x_s <x_sherl@163.com>
2017-11-28 22:46:46 +0800

    订单项DAO

---
+++ src/com/tmall/beans/OrderItem.java
@@ -0,0 +1,42 @@
+package com.tmall.beans;
+
+public class OrderItem{
+	
+	private int id;
+	private Product product;
+	private Order order;
+	private User user;
+	private int number;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Order getOrder() {
+		return order;
+	}
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
