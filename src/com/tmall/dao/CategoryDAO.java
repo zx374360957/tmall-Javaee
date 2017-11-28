57dd91beb7c76ca58a5e76fa84ede1ac7538b162
x_s <x_sherl@163.com>
2017-11-28 22:42:35 +0800

    分类DAO

---
+++ src/com/tmall/dao/CategoryDAO.java
@@ -0,0 +1,132 @@
+package com.tmall.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tmall.beans.Category;
+import com.tmall.util.DBUtil;
+
+public class CategoryDAO {
+	public int getTotal() {
+		int total = 0;
+		try {
+			Connection c = DBUtil.getConnection();
+			Statement s = c.createStatement();
+			String sql = "SELECT count(*) FROM category";
+
+			ResultSet rs = s.executeQuery(sql);
+			if (rs.next()) {
+				total = rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+
+	public void add(Category bean) {
+		try {
+			String sql = "INSERT INTO category(id, name) VALUES (null, ?)";
+			Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, bean.getName());
+
+			ps.executeUpdate();
+
+			ResultSet rs = ps.getGeneratedKeys();
+			if (rs.next()) {
+				int id = rs.getInt(1);
+				bean.setId(id);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void update(Category bean) {
+		try {
+			String sql = "UPDATE category SET name = ? WHERE id = ?";
+			Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql);
+
+			ps.setString(1, bean.getName());
+			ps.setInt(2, bean.getId());
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Category get(int id) {
+		Category category = null;
+		try {
+			String sql = "SELECT * FROM category WHERE id = ?";
+			Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				category = new Category();
+				category.setId(id);
+				category.setName(rs.getString("name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return category;
+	}
+
+	public void dalete(int id) {
+		try {
+			String sql = "DELETE FROM category WHERE id = ?";
+			Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, id);
+
+			ps.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Category> list() {
+		return list(0, Short.MAX_VALUE);
+	}
+
+	public List<Category> list(int beg, int len) {
+		List<Category> ls = new ArrayList<Category>();
+		try {
+			String sql = "SELECT * FROM category LIMIT ?,?";
+			Connection c = DBUtil.getConnection();
+			PreparedStatement ps = c.prepareStatement(sql);
+			ps.setInt(1, beg);
+			ps.setInt(2, len);
+
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Category cg = new Category();
+				cg.setId(rs.getInt("id"));
+				cg.setName(rs.getString("name"));
+				ls.add(cg);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return ls;
+	}
+
+	public static void main(String[] args) {
+		CategoryDAO c = new CategoryDAO();
+		System.out.println(c.getTotal());
+	}
+}
