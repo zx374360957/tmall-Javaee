79d19de33397d8a3e800b2ae20287580402f867a
x_s <x_sherl@163.com>
2017-11-28 22:44:15 +0800

    编码filter

---
+++ src/com/tmall/filter/EncodingFilter.java
@@ -0,0 +1,28 @@
+package com.tmall.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+	public void init(FilterConfig config) {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+	}
+
+	public void destory() {
+
+	}
+
+}
