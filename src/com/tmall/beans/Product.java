64b28fb0950c8e927ae06f5ef5280751ace3ca15
x_s <x_sherl@163.com>
2017-11-28 22:47:52 +0800

    产品bean

---
+++ src/com/tmall/beans/Product.java
@@ -0,0 +1,126 @@
+package com.tmall.beans;
+
+import java.util.Date;
+import java.util.List;
+
+public class Product{
+	
+	private int id;
+	private String name;
+	private String subTitle;
+	private float orignalPrice;
+	private float promotePrice;
+	private int stock;
+	private Date createDate;
+	private Category category;
+	private ProductImage firstProductImage;
+	private List<ProductImage> productImages;
+	private List<ProductImage> productSingleImages;
+	private List<ProductImage> productDetailsImages;
+	private int reviewCount;
+	private int saleCount;
+	
+	@Override
+	public String toString() {
+		return name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubTitle() {
+		return subTitle;
+	}
+	public void setSubTitle(String subTitle) {
+		this.subTitle = subTitle;
+	}
+	public float getOrignalPrice() {
+		return orignalPrice;
+	}
+	public void setOrignalPrice(float orignalPrice) {
+		this.orignalPrice = orignalPrice;
+	}
+	public float getPromotePrice() {
+		return promotePrice;
+	}
+	public void setPromotePrice(float promotePrice) {
+		this.promotePrice = promotePrice;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public ProductImage getFirstProductImage() {
+		return firstProductImage;
+	}
+
+	public void setFirstProductImage(ProductImage firstProductImage) {
+		this.firstProductImage = firstProductImage;
+	}
+
+	public List<ProductImage> getProductImages() {
+		return productImages;
+	}
+
+	public void setProductImages(List<ProductImage> productImages) {
+		this.productImages = productImages;
+	}
+
+	public List<ProductImage> getProductSingleImages() {
+		return productSingleImages;
+	}
+
+	public void setProductSingleImages(List<ProductImage> productSingleImages) {
+		this.productSingleImages = productSingleImages;
+	}
+
+	public List<ProductImage> getProductDetailsImages() {
+		return productDetailsImages;
+	}
+
+	public void setProductDetailsImages(List<ProductImage> productDetailsImages) {
+		this.productDetailsImages = productDetailsImages;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	public int getSaleCount() {
+		return saleCount;
+	}
+
+	public void setSaleCount(int saleCount) {
+		this.saleCount = saleCount;
+	}
+}
